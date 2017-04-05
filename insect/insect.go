998942e4f3c8be606743962e631e6e10678ce5ad
lys091112 <lys091112@gmail.com>
2017-04-05 23:30:03 +0800

    add client demo

--- insect/insect.go
+++ insect/insect.go
@@ -5,37 +5,48 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"net/tl"
+	"time"
 )
 
+/**
+* 遇到的问题
+* http://studygolang.com/articles/7692
+* http://stackoverflow.com/questions/16280176/go-panic-runtime-error-invalid-memory-address-or-nil-pointer-dereference
+* TLS handshake timeout
+ */
 func download(url string) string {
 	defer func() {
 		if r := recover(); r != nil {
-			log.Println("[E]", r)
+			log.Fatalf("[E] %v ", r)
 		}
 	}()
 
+	startTime := time.Now().UnixNano()
 	req, _ := http.NewRequest("GET", url, nil)
 	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36")
 	req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
 	client := http.DefaultClient
-	tr := &http.Transport{
-		TLSClientConfig:&tls.Con
-	}
+	client.Timeout = 60 * time.Second
 	res, e := client.Do(req)
-	//	res, e := http.Get(url)
+	fmt.Println()
 	if e != nil {
-		fmt.Errorf("get %s error: %s", url, e)
+		cost := time.Now().UnixNano() - startTime
+		fmt.Printf("cost time is %d", cost/(1000000000))
+		fmt.Println()
+		log.Fatalf("get %s error: %s", url, e)
+		fmt.Println()
 	}
 	fmt.Println("-------" + url)
+	body := res.Body
+	defer body.Close()
 
 	if res.StatusCode == 200 {
-		body := res.Body
-		defer body.Close()
 		bodyBytes, _ := ioutil.ReadAll(body)
 		str := string(bodyBytes)
-		fmt.Print(str)
+		fmt.Print("content", str)
 	}
+	cost := time.Now().UnixNano() - startTime
+	fmt.Printf("cost time is %d", cost/(1000000000))
 	return "hello"
 }
 
